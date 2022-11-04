e85ae60eeecf06d208205a86bcd37ca05f07d2ff
Jody-septiawan <jody.septiawan5@gmail.com>
2022-11-04 16:14:46 +0700

    Animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
-import './src/screens/Home.dart';
-import './src/screens/Counter.dart';
+import './src/screens/AnimatedContainer.dart';
+import './src/screens/AnimatedPosition.dart';
 
 void main() {
-  runApp(const MyWidget());
+  runApp(const MyApp());
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetList = [
+    AnimatedContainerPage(),
+    AnimatedPositionPage()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Scaffold(
+        body: SafeArea(child: _widgetList.elementAt(_selectedIndex)),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.wrap_text), label: 'Container'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.drag_handle), label: 'Position')
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
+      ),
     );
   }
 }
